e5896b7111cdbb0001c93268fae19fa248305dad
Phil Nelson <phil@extrafuture.com>
2012-03-10 11:20:52 -0800

    Rain.

--- main.lua
+++ main.lua
@@ -2,6 +2,8 @@ function love.load()
 	sun = {}
 	sun['x'] = 10
 	sun['y'] = 10
+	
+	wind = 'e'
 
 	grass_size = 120
 	
@@ -9,7 +11,13 @@ function love.load()
 	
 	day_time = love.timer.getMicroTime( )
 	
-	time_step = 1000
+	time_step = 1
+	
+	colors = {}
+	colors['empty_dirt'] = {35,24,17}
+	colors['dirt'] = {110,85,56}
+	colors['seed'] = {117,137,24}
+	colors['rain'] = {0,0,255}
 
    radicles = {}   -- table which will hold our stars
    max_size = 100   -- how many stars we want
@@ -42,16 +50,17 @@ function love.draw()
 
 	day_time = love.timer.getTime( )
 	next_time = love.timer.getMicroTime( )
-	love.graphics.print( day_time, 120, 120, 0, 1, 1 )
+	love.graphics.print( string.sub(day_time, 0,2), 700, 20, 0, 1, 1 )
 	
-	if next_time-sun_time > 1/time_step then 
-		sun['x'] = sun['x'] + .5
+	
+	if next_time-sun_time > time_step then 
+		sun['x'] = sun['x'] + 1
 		sun_time = love.timer.getMicroTime( )
 	end
 
 	love.graphics.setBackgroundColor( 103, 195, 245 )
 	
-	love.graphics.setColor( 86,67,52, 255 )
+	love.graphics.setColor(colors['dirt'])
 	love.graphics.rectangle( 'fill', 0, (love.graphics.getHeight()-grass_size), love.graphics.getWidth(), grass_size )
 	
 	if(sun['x'] < love.graphics.getWidth()+40) then
@@ -67,9 +76,32 @@ function love.draw()
 	   love.graphics.point(dirt_dots[i][1], dirt_dots[i][2])   -- draw each point
 	end
 	
-	love.graphics.setColor(87,47,36, 255 )
-	for i=1, #more_dirt_dots do   -- loop through all of our dirt
-	   love.graphics.point(more_dirt_dots[i][1], more_dirt_dots[i][2])   -- draw each point
-	end
+	-- Draw Seed
+	love.graphics.setColor(colors['seed'])
+	love.graphics.point(120, love.graphics.getHeight()-(grass_size-20))
+	love.graphics.point(122, love.graphics.getHeight()-(grass_size-20))
+	love.graphics.point(120, love.graphics.getHeight()-(grass_size-22))
+	love.graphics.point(122, love.graphics.getHeight()-(grass_size-22))
+	
+	love.graphics.setColor(colors['rain'])
+	for i=1, 60 do   -- generate the coords of our dirt specks
+		local x = math.random(0, love.graphics.getWidth()-1)   -- generate a "random" number for the x coord of this star
+		local y = math.random(0, love.graphics.getHeight()-1)   -- both coords are limited to the screen size, minus 5 pixels of padding
+		
+		x2 = x
+		y2 = y+5
+		
+		if wind == 'w' then
+			x2 = x+10
+			y2 = y+10
+		end
+		
+		if wind == 'e' then
+			x2 = x-10
+			y2 = y+10
+		end
 
+		love.graphics.line( x, y, x2, y2)
+   end
+	
 end
